d1efcc972c392439d6878a74a75730d4e7239b84
ufin <torjenymo@hotmail.com>
2012-10-23 23:59:15 +0200

    simserviceimpl.class endret på utregning, for jamnere forbruk med hus med mange beboere endring i "hourlypowerConsumtion"7 torje

--- banan/src/com/banan/server/SimServiceImpl.java
+++ banan/src/com/banan/server/SimServiceImpl.java
@@ -49,9 +49,9 @@ public class SimServiceImpl extends RemoteServiceServlet implements SimService
 		for (int i = 0; i < 24; i++)
 		{
 			int res = 100;
-			res *= byggårForbruksFaktor(Integer.parseInt(p.getBuildYear()));
+			res*= byggårForbruksFaktor(Integer.parseInt(p.getBuildYear()));
 			res*= beboereForbruksFaktor(Integer.parseInt(p.getHouseResidents()));
-			res*= this.hourlyPowerConsumption(i);
+			res*= this.hourlyPowerConsumption(i, Integer.parseInt(p.getHouseResidents()));
 			resultat[i] = res;
 		}
 		
@@ -83,61 +83,91 @@ public class SimServiceImpl extends RemoteServiceServlet implements SimService
 	{
 		if (beboere == 1)
 		{
-			return 0.5;			
+			return 1.0;			
 		}
 		else if(beboere == 2)
 		{
-			return 0.6;
+			return 1.25;
 		}
 		else if(beboere == 3)
 		{
-			return 0.7;
+			return 1.35;
 		}
 		else if(beboere == 4)
 		{
-			return 0.8;
+			return 1.45;
 		}
 		else if(beboere == 5)
 		{
-			return 0.85;
+			return 1.6;
 		}
 		else if(beboere == 6)
 		{
-			return 0.9;
+			return 1.80;
 		}
 		else if(beboere > 6)
 		{
-			return 0.95;
+			return 2.0;
 		}
 		else return 1;
 	}
-	public double hourlyPowerConsumption(int time)
+	
+	public double hourlyPowerConsumption(int time, int beboere)
 	{
 		double[] powerConsumption = new double[24];
-		powerConsumption[0] = 0.40;
-		powerConsumption[1] = 0.35;
-		powerConsumption[2] = 0.35;
-		powerConsumption[3] = 0.30;
-		powerConsumption[4] = 0.25;
-		powerConsumption[5] = 0.35;
-		powerConsumption[6] = 0.50;
+		double residentsFactor = 0;
+		
+		if (beboere == 1)
+		{
+			residentsFactor = 1.0;
+		}
+		else if (beboere == 2)
+		{
+			residentsFactor = 1.05;
+		}
+		else if (beboere == 3)
+		{
+			residentsFactor = 1.11;
+		}
+		else if (beboere == 4)
+		{
+			residentsFactor = 1.17;
+		}
+		else if (beboere > 4)
+		{
+			residentsFactor = 1.25;
+		}
+		else {
+			residentsFactor = 100.0;  // feil
+		}
+		double faktorJustering = 0.90;
+		
+		//jamnere strømforbruk med flere folk i huset
+		// eks. 1 beboer bruker kl. 0000 20kwt og 1600 50kwt, med 5 i huset bruker samme hus 50kwt og 100kwt  (4/10 vs 5/10)
+		powerConsumption[0] = 0.40 * residentsFactor;
+		powerConsumption[1] = 0.35 * residentsFactor;
+		powerConsumption[2] = 0.35 * residentsFactor;
+		powerConsumption[3] = 0.30 * residentsFactor;
+		powerConsumption[4] = 0.25 * residentsFactor;
+		powerConsumption[5] = 0.35 * residentsFactor;
+		powerConsumption[6] = 0.50 * residentsFactor;
 		powerConsumption[7] = 0.95;
-		powerConsumption[8] = 0.80;
-		powerConsumption[9] = 0.75;
-		powerConsumption[10] = 0.70;
-		powerConsumption[11] = 0.60;
-		powerConsumption[12] = 0.65;
-		powerConsumption[13] = 0.70;
-		powerConsumption[14] = 0.65;
-		powerConsumption[15] = 0.80;
+		powerConsumption[8] = 0.65;
+		powerConsumption[9] = 0.50;
+		powerConsumption[10] = 0.48;
+		powerConsumption[11] = 0.50;
+		powerConsumption[12] = 0.52 * (residentsFactor/faktorJustering);
+		powerConsumption[13] = 0.54 * (residentsFactor/faktorJustering);
+		powerConsumption[14] = 0.55 * (residentsFactor/faktorJustering);
+		powerConsumption[15] = 0.60 * (residentsFactor/faktorJustering);
 		powerConsumption[16] = 1.00;
 		powerConsumption[17] = 0.95;
-		powerConsumption[18] = 0.90;
-		powerConsumption[19] = 0.80;
-		powerConsumption[20] = 0.70;
-		powerConsumption[21] = 0.80;
-		powerConsumption[22] = 0.70;
-		powerConsumption[23] = 0.50;
+		powerConsumption[18] = 0.67 * (residentsFactor/faktorJustering);
+		powerConsumption[19] = 0.65 * (residentsFactor/faktorJustering);
+		powerConsumption[20] = 0.63 * (residentsFactor/faktorJustering);
+		powerConsumption[21] = 0.61 * (residentsFactor/faktorJustering);
+		powerConsumption[22] = 0.58 * (residentsFactor/faktorJustering);
+		powerConsumption[23] = 0.50 * (residentsFactor/faktorJustering);
 		
 		return powerConsumption[time];
 	}
