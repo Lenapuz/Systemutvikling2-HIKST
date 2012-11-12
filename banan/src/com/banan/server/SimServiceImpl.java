a739ef9606b7c07c6e44b08fde038c65ae287bca
ufin <torje@nymobil.no>
2012-11-12 21:43:14 +0100

    lagt til "hourlyHeating" som tar for seg endringer i bruk av oppvarmingsstrøm gjennom døgnet. Endret på utregning også. Pluss store endringer i hourlyConsumption. Simuleringen vil nå gi 0 i strømforbruk til oppvarming ved temp. 20 og opp (slik det var ment). Å forbruks strøm er en egen post som endres etter egne variabler gjennom døgnet.

--- banan/src/com/banan/server/SimServiceImpl.java
+++ banan/src/com/banan/server/SimServiceImpl.java
@@ -72,13 +72,15 @@ public class SimServiceImpl extends RemoteServiceServlet implements SimService
 			} 
 			else 
 			{		
-				
-				//int res = gjsnittligForbrukPrKvm *  Integer.parseInt(p.getHouseSize());
 				res += getOppvarmingsForbrukPerKvm(temperatur,Integer.parseInt(p.getBuildYear()));  //varmeforbrukperKVM mot varmetaphus
-				//res *=h.getheatFactor(); //varmefaktor fra oppvarming
-				res *= gjsnittligForbrukApparater;   // snittstrømforbruk på annet enn oppvarming jamfør NVE 2012 energirapport
-				res *= this.hourlyPowerConsumption(i, Integer.parseInt(p.getHouseResidents()));	// ganger med forbruksendring gjennom døgnet (ssb)
 				res *= Integer.parseInt(p.getHouseSize()); // kvm
+				res -= Integer.parseInt(p.getHouseSize()); //fjerne res = 1w per kvm fra utregningen
+				res *= this.hourlyHeating(i); // Endringer i oppvarming gjennom døgnet
+				res += this.hourlyPowerConsumption(i, Integer.parseInt(p.getHouseResidents()));	// Endringer i lys og el.app. bruk gjennom døgnet 
+				//res *=h.getheatFactor(); //varmefaktor fra oppvarming
+				//res *= gjsnittligForbrukApparater;   // snittstrømforbruk på annet enn oppvarming jamfør NVE 2012 energirapport
+				//int res = gjsnittligForbrukPrKvm *  Integer.parseInt(p.getHouseSize());
+				
 				
 				resultat[i] = (int)res;
 				res = 1;
@@ -105,7 +107,75 @@ public class SimServiceImpl extends RemoteServiceServlet implements SimService
 		int res = tempCelsius + 273;
 		return res;
 	}
+	
+	public double hourlyHeating(int time)
+	{
+		double[] heating = new double[24];
+		heating[0] = 0.75;
+		heating[1] = 0.75;
+		heating[2] = 0.75;
+		heating[3] = 0.75;
+		heating[4] = 0.75;
+		heating[5] = 0.75;
+		heating[6] = 0.75;
+		heating[7] = 0.80;
+		heating[8] = 1.00;
+		heating[9] = 1.00;
+		heating[10] = 1.00;
+		heating[11] = 1.00;
+		heating[12] = 1.00;
+		heating[13] = 1.00;
+		heating[14] = 1.00;
+		heating[15] = 1.00;
+		heating[16] = 1.00;
+		heating[17] = 1.00;
+		heating[18] = 1.00; 
+		heating[19] = 1.00;
+		heating[20] = 1.00;
+		heating[21] = 1.00;
+		heating[22] = 1.00;
+		heating[23] = 0.80;
+		
+		return heating[time];
+	}
+	
 	public double hourlyPowerConsumption(int time, int beboere)
+	{
+		double[] powerConsumption = new double[24];
+		double beboerFaktorMin = 0;
+		double beboerFaktorMax = 0;
+	
+	if (beboere == 1)
+	{
+		beboerFaktorMin = 550;	
+		beboerFaktorMax = 450;
+	}
+	else if (beboere == 2)
+	{
+		beboerFaktorMin = 680;  // gjennomsnitts husstand, Max = Min = Max 
+		beboerFaktorMax = 680;	// 680 watt lys&el.app. 34% av 2000w (ca.snitt "ssb" per time)
+	}
+	else if (beboere == 3 )
+	{
+		beboerFaktorMin = 890;
+		beboerFaktorMax = 900;
+	}
+	else if (beboere == 4)
+	{
+		beboerFaktorMin = 1000;
+		beboerFaktorMax = 1200;
+	}
+	else if (beboere == 5)
+	{
+		beboerFaktorMin = 1100;
+		beboerFaktorMax = 1300;
+	}
+	else {
+		beboerFaktorMin =  1200; 	
+		beboerFaktorMax =  1500;  
+	}
+	
+	/*public double hourlyPowerConsumption(int time, int beboere)
 	{
 		double[] powerConsumption = new double[24];
 		double beboerFaktorMin = 0;
@@ -139,7 +209,7 @@ public class SimServiceImpl extends RemoteServiceServlet implements SimService
 		else {
 			beboerFaktorMin =  1.65; 	
 			beboerFaktorMax =  1.93;  
-		}
+		}*/
 		
 		// PowerConsumption tall er gitt snitt forbruk av strøm fra time til time. (gjennomsnitts husstand 2 personer)
 		
@@ -148,30 +218,30 @@ public class SimServiceImpl extends RemoteServiceServlet implements SimService
 		
 		// beboerFaktorMax - Tiden av døgnet hvor det er mest forskjell mellom en eller flere
 		// beboere, gitt i snitt. 
-		powerConsumption[0] = 0.86 * beboerFaktorMin;
-		powerConsumption[1] = 0.66 * beboerFaktorMax;
-		powerConsumption[2] = 0.61 * beboerFaktorMax;
-		powerConsumption[3] = 0.59 * beboerFaktorMax;
-		powerConsumption[4] = 0.58 * beboerFaktorMax;
-		powerConsumption[5] = 0.59 * beboerFaktorMax;
-		powerConsumption[6] = 0.60 * beboerFaktorMax;
-		powerConsumption[7] = 0.67 * beboerFaktorMax;
-		powerConsumption[8] = 0.89 * beboerFaktorMin;
-		powerConsumption[9] = 0.91 * beboerFaktorMin;
-		powerConsumption[10] = 0.92 * beboerFaktorMin;
-		powerConsumption[11] = 0.90 * beboerFaktorMin;
-		powerConsumption[12] = 0.70 * beboerFaktorMax;
-		powerConsumption[13] = 0.69 * beboerFaktorMax;
-		powerConsumption[14] = 0.69 * beboerFaktorMax;
-		powerConsumption[15] = 0.71 * beboerFaktorMax;
-		powerConsumption[16] = 0.91 * beboerFaktorMin;
-		powerConsumption[17] = 0.95 * beboerFaktorMin;
-		powerConsumption[18] = 0.96 * beboerFaktorMin;
-		powerConsumption[19] = 0.98 * beboerFaktorMin;
-		powerConsumption[20] = 0.99 * beboerFaktorMin;
+		powerConsumption[0] = 0.72 * beboerFaktorMin;
+		powerConsumption[1] = 0.38 * beboerFaktorMax;
+		powerConsumption[2] = 0.33 * beboerFaktorMax;
+		powerConsumption[3] = 0.24 * beboerFaktorMax;
+		powerConsumption[4] = 0.23 * beboerFaktorMax;
+		powerConsumption[5] = 0.24 * beboerFaktorMax;
+		powerConsumption[6] = 0.32 * beboerFaktorMax;
+		powerConsumption[7] = 0.39 * beboerFaktorMax;
+		powerConsumption[8] = 0.75 * beboerFaktorMin;
+		powerConsumption[9] = 0.84 * beboerFaktorMin;
+		powerConsumption[10] = 0.85 * beboerFaktorMin;
+		powerConsumption[11] = 0.83 * beboerFaktorMin;
+		powerConsumption[12] = 0.59 * beboerFaktorMax;
+		powerConsumption[13] = 0.43 * beboerFaktorMax;
+		powerConsumption[14] = 0.43 * beboerFaktorMax;
+		powerConsumption[15] = 0.50 * beboerFaktorMax;
+		powerConsumption[16] = 0.84 * beboerFaktorMin;
+		powerConsumption[17] = 0.88 * beboerFaktorMin;
+		powerConsumption[18] = 0.89 * beboerFaktorMin;
+		powerConsumption[19] = 0.91 * beboerFaktorMin;
+		powerConsumption[20] = 0.92 * beboerFaktorMin;
 		powerConsumption[21] = 1.00 * beboerFaktorMin;
-		powerConsumption[22] = 0.99 * beboerFaktorMin;
-		powerConsumption[23] = 0.95 * beboerFaktorMin;
+		powerConsumption[22] = 0.92 * beboerFaktorMin;
+		powerConsumption[23] = 0.88 * beboerFaktorMin;
 		
 		return powerConsumption[time];
 	}
